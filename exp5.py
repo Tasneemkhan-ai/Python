54b4594aef8fb320c5715887d49860e8a5d3d684
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 08:34:36 +0530

    exp5.py

--- exp5.py
+++ exp5.py
@@ -1,7 +1,23 @@
 #Aim:To manage task list using list and tuple
 #Coder:Tasneem khan
 #Date: 21-01-2026
-Task=["Wake up","Freshen up","Have breakfast","GO to college","SLeep"]
-print(Task)
-print(Task.append("Wake up"))
+print("--- Task List Manager ---")
 
+tasks = ["Sleep", "Getup", "Brush"]
+print(tasks)
+
+newTask = input("Enter a new task to add: ")
+tasks.append(newTask)
+print("Tasks after Adding:",tasks)
+
+num= int(input("Enter the index of the task to edit: "))
+newTask = input("Enter the new task: ")
+tasks[num] = newTask
+print("Updated tasks:",tasks)
+
+index = int(input("Enter the index of the task to remove: "))
+tasks.pop(index)
+print("Tasks after Removing:",tasks)
+
+tasks.sort()
+print(f"Tasks after Sorting: {tasks}")
