dee2a37a8b0e2d9ccd68ef961cd9fa8ac17c0ecd
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 08:21:25 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,11 @@
+# AIM: Create a personalized greeting message 
+# Coder: Tasneem Khan
+# Date: 5/1/26
+
+# Write your code here
+name=input("Enter Name: ")
+age=input("Enter Age: ")
+
+print("Welcome to Rizvi College of Engineering")
+print("Hello,",name)
+print("You are",age,"years old.")
