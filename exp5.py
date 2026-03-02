fe23e3e776b0adedb62cbc31492a03b7dfbceb80
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 08:51:44 +0530

    Task list using list

--- exp5.py
+++ exp5.py
@@ -2,22 +2,17 @@
 #Coder:Tasneem khan
 #Date: 21-01-2026
 print("--- Task List Manager ---")
-
 tasks = ["Sleep", "Getup", "Brush"]
 print(tasks)
-
 newTask = input("Enter a new task to add: ")
-tasks.append(newTask)
+tasks.append(newTask)    #to add an element
 print("Tasks after Adding:",tasks)
-
-num= int(input("Enter the index of the task to edit: "))
-newTask = input("Enter the new task: ")
+num= int(input("Enter the index of the task to edit: "))  
+newTask = input("Enter the new task: ")  #to edit an element
 tasks[num] = newTask
 print("Updated tasks:",tasks)
-
 index = int(input("Enter the index of the task to remove: "))
-tasks.pop(index)
+tasks.pop(index)   #to remove the element
 print("Tasks after Removing:",tasks)
-
-tasks.sort()
+tasks.sort()      #to sort out the list 
 print(f"Tasks after Sorting: {tasks}")
