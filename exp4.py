3793a068c1471504c8af26dca1a233e55f9bcd6b
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 09:12:35 +0530

    Calc of simple interest

--- exp4.py
+++ exp4.py
@@ -2,12 +2,8 @@
 #Coder: Tasneem Khan 
 #Date: 19-01-2026
 print("---Simple Interest Calculator---")
-#To take principal from the user
-principal=float(input("Enter the principal:"))
-#To take interest from the user
-intrst=float(input("Enter the rate of interest:"))
-#TO take time period from the user
-time=float(input("Enter the time period(in years):"))
-#formula of simple interest
-SI=(principal*intrst*time)/100
+principal=float(input("Enter the principal:"))    #To take principal from the user
+intrst=float(input("Enter the rate of interest:"))   #To take interest from the user
+time=float(input("Enter the time period(in years):"))    #TO take time period from the user
+SI=(principal*intrst*time)/100                 #formula of simple interest
 print("Simple interst =",SI)
