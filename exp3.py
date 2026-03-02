b9b48fc2f8ed757216ffae896fb264f8e67372b4
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 09:07:49 +0530

    Refactor comments and improve code readability

--- exp3.py
+++ exp3.py
@@ -1,16 +1,15 @@
-#Aim: To calculate gross salary from basic salary, daily allowance, 
-#     house rent allowance, travel allowance
+#Aim: To calculate gross salary from basic salary, daily allowance, house rent allowance, travel allowance
 #Coder: Tasnereem Khan
-#date: 17-01-2026
+#Date: 17-01-2026
 print("- Gross Salary Calculator-")
 #To get base salary from user
 bs=float(input("Enter your base salary:"))
 print("Basic salary is",bs)
-da=0.7*bs
+da=0.7*bs                #daily allowance
 print("Daily allowance is ",da)
-ta=0.3*bs
+ta=0.3*bs                  #travel allowance
 print("Travel allowance is",ta)
-hra=0.1*bs
+hra=0.1*bs                 #house rent allowance
 print("House rent allowance is",hra)
-gross_salary=bs+da+ta+hra 
+gross_salary=bs+da+ta+hra     #calculation of gross salary
 print("Gross salary is",gross_salary)
