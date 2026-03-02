cb5e057aa0d4ca486819305c646f017732683e4c
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 08:25:53 +0530

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,9 @@
+# AIM: Write a python program to calculate area of rectangle.
+# Coder: Tasneem Khan
+# Date: 14/1/26
+
+# Write your code here
+l=float(input("Enter Length: "))
+w=float(input("Enter Width: "))
+area=l*w		#area of rectangle is length times width
+print("Area of Rectangle = ",area)
