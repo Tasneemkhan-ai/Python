8de05267f4ebda20f5950eb4a94c03603a3dd83c
Tasneemkhan-ai <khantasneem647@gmail.com>
2026-03-02 08:27:18 +0530

    Add files via upload

---
+++ exp5.py
@@ -0,0 +1,7 @@
+#Aim:To manage task list using list and tuple
+#Coder:Tasneem khan
+#Date: 21-01-2026
+Task=["Wake up","Freshen up","Have breakfast","GO to college","SLeep"]
+print(Task)
+print(Task.append("Wake up"))
+
